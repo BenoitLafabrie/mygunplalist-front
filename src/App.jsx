b65d9fee101889e682d80fe2154bb3395a5c9a36
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2024-01-16 18:48:33 +0100

    Minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import { UserContextProvider } from "./context/User.jsx";
 import Home from "./pages/Home.jsx";
 import Root from "./pages/Root.jsx";
@@ -11,7 +9,7 @@ import Search from "./pages/Search.jsx";
 import Wishlist from "./pages/Wishlist.jsx";
 import About from "./pages/About.jsx";
 import TermsOfUse from "./pages/TermsOfUse.jsx";
-// import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
+import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/react";
@@ -48,7 +46,7 @@ const router = createBrowserRouter([
       { path: "/login", element: <Login /> },
       { path: "/about", element: <About /> },
       { path: "/terms-of-use", element: <TermsOfUse /> },
-      // { path: "/privacy-policy", element: <PrivacyPolicy /> },
+      { path: "/privacy-policy", element: <PrivacyPolicy /> },
     ],
   },
   {
