f822552b9729d7c5bc76f8ffd985bd616b806384
BenoitLafabrie <[benoit_lafabrie_pro@fastmail.com](mailto:benoit_lafabrie_pro@fastmail.com)>
2024-03-19 15:19:36 +0100

    Transition from cards to table complete, updated item status change methods to fit that display and handling multiple changes at once.

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,14 +1,13 @@
 import { ChevronDownIcon, ChevronUpIcon } from "@chakra-ui/icons";
 import {
   Box,
-  CardFooter,
   Center,
+  Checkbox,
   Link as ChakraLink,
   HStack,
   Heading,
   Image,
   Select,
-  SimpleGrid,
   Stack,
   Table,
   TableCaption,
@@ -26,7 +25,6 @@ import { useContext, useEffect, useState } from "react";
 import { BiBarcodeReader, BiSolidShareAlt } from "react-icons/bi";
 import { Link as ReactRouterLink } from "react-router-dom";
 import { updateItemStatus } from "../api/myGunplaList";
-import { KitCard } from "../components/KitCard";
 import Loading from "../components/Loading";
 import Pagination from "../components/Pagination";
 import { UserContext } from "../context/User";
@@ -42,7 +40,7 @@ export default function Collection() {
   const toast = useToast();
   console.log(myGunplaList);
 
-  const handleStatusChange = async (item, newStatus) => {
+  const handleStatusChange = async (item, newStatus, showToast = true) => {
     try {
       const item_status_id = item?.Item_status[0]?.item_status_id;
 
@@ -60,24 +58,48 @@ export default function Collection() {
       // Update myGunplaList.Items with the new array
       setMyGunplaList({ ...myGunplaList, Items: newItems });
 
-      toast({
-        title: "Statut mis à jour",
-        description: "Le status du kit a bien été modifié",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
+      if (showToast) {
+        toast({
+          title: "Statut mis à jour",
+          description: "Le status du kit a bien été modifié",
+          status: "success",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
     } catch (error) {
-      toast({
-        title: "Oups",
-        description:
-          "Impossible de mettre à jour le statut du kit, réessayez plus tard",
-        status: "error",
-        duration: 3000,
-        isClosable: true,
-      });
+      if (showToast) {
+        toast({
+          title: "Oups",
+          description:
+            "Impossible de mettre à jour le statut du kit, réessayez plus tard",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
     }
   };
+
+  const handleAllStatusChange = async (newStatus) => {
+    for (const id of selectedRows) {
+      const item = currentItems.find((item) => item.item_id === id);
+      console.log(item);
+      if (item) {
+        await handleStatusChange(item, newStatus, false);
+      }
+    }
+    setIsAllSelected(false);
+    setSelectedRows([]);
+    toast({
+      title: "Statut mis à jour",
+      description: "Le status des kits a bien été modifié",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
+  };
+
   const [totalKits, setTotalKits] = useState(0);
   const [garageKits, setGarageKits] = useState(0);
   const [deployedKits, setDeployedKits] = useState(0);
@@ -87,6 +109,9 @@ export default function Collection() {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const currentItems = filteredItems?.slice(startIndex, endIndex);
+  const [selectValue, setSelectValue] = useState("");
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [isAllSelected, setIsAllSelected] = useState(false);
 
   const [sortField, setSortField] = useState(null);
   const [sortDirection, setSortDirection] = useState("asc");
@@ -235,57 +260,73 @@ export default function Collection() {
             </VStack>
           </HStack>
         </Box>
-        {/*         <SimpleGrid
-          spacing={4}
-          templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
-          mx="1em"
-          mb="1em"
-        >
-          {currentItems.map((item) => (
-            <KitCard key={item.item_id} item={item}>
-              <CardFooter mt="-1em" p="0.5em">
-                <Select
-                  colorScheme="brand"
-                  size="sm"
-                  defaultValue={item?.Item_status[0]?.status}
-                  onChange={(event) =>
-                    handleStatusChange(item, event.target.value)
-                  }
-                >
-                  <option value="Garage">Hangar</option>
-                  <option value="Assembling">Assemblage</option>
-                  <option value="Deployed">Déployé</option>
-                </Select>
-              </CardFooter>
-            </KitCard>
-          ))}
-        </SimpleGrid> */}
         <Stack w="100%">
           <TableContainer m="2%">
             <Table
-              size={{ base: "sm" }}
+              size={{ base: "sm", md: "md" }}
               variant="striped"
               colorScheme="brand"
               w="100%"
             >
               <TableCaption placement="top" m={0}>
-                <HStack spacing={2} my="1em">
+                <HStack spacing={1.5} my="1em">
                   <Box w="4" h="4" borderRadius="2" bg="#005778" />
-                  <Text fontSize="sm" fontWeight="400">
+                  <Text fontSize="xs" fontWeight="400">
                     Hangar
                   </Text>
                   <Box w="4" h="4" borderRadius="2" bg="#FF9300" />
-                  <Text fontSize="sm" fontWeight="400">
+                  <Text fontSize="xs" fontWeight="400">
                     Assemblage
                   </Text>
                   <Box w="4" h="4" borderRadius="2" bg="#A4DD70" />
-                  <Text fontSize="sm" fontWeight="400">
+                  <Text fontSize="xs" fontWeight="400">
                     Déployé
                   </Text>
+                  <Select
+                    width={{ base: "100px", md: "150px" }}
+                    value={selectValue}
+                    placeholder="Statut"
+                    colorScheme="brand"
+                    size={{ base: "xs", md: "sm" }}
+                    onChange={(event) => {
+                      if (selectedRows.length === 1) {
+                        const item = currentItems.find(
+                          (item) => item.item_id === selectedRows[0]
+                        );
+                        if (item) {
+                          handleStatusChange(item, event.target.value);
+                        }
+                      } else {
+                        handleAllStatusChange(event.target.value);
+                      }
+                      setSelectValue("");
+                    }}
+                  >
+                    <option value="Garage">Hangar</option>
+                    <option value="Assembling">Assemblage</option>
+                    <option value="Deployed">Déployé</option>
+                  </Select>
                 </HStack>
               </TableCaption>
               <Thead>
                 <Tr>
+                  <Th>
+                    <Checkbox
+                      size={{ base: "sm", md: "md" }}
+                      colorScheme="brand"
+                      isChecked={isAllSelected}
+                      onChange={(e) => {
+                        setIsAllSelected(e.target.checked);
+                        if (e.target.checked) {
+                          setSelectedRows(
+                            filteredItems.map((item) => item.item_id)
+                          );
+                        } else {
+                          setSelectedRows([]);
+                        }
+                      }}
+                    />
+                  </Th>
                   <Th textAlign="center">Box Art</Th>
                   <Th px={1} py={2} onClick={() => requestSort("name")}>
                     Nom
@@ -306,6 +347,31 @@ export default function Collection() {
                 {currentItems.map((item) => {
                   return (
                     <Tr key={item.item_id} p={2}>
+                      <Td px={1}>
+                        <Box
+                          display="flex"
+                          alignItems="center"
+                          justifyContent="center"
+                        >
+                          <Checkbox
+                            size={{ base: "sm", md: "md" }}
+                            colorScheme="brand"
+                            isChecked={selectedRows.includes(item.item_id)}
+                            onChange={(e) => {
+                              if (e.target.checked) {
+                                setSelectedRows((prev) => [
+                                  ...prev,
+                                  item.item_id,
+                                ]);
+                              } else {
+                                setSelectedRows((prev) =>
+                                  prev.filter((id) => id !== item.item_id)
+                                );
+                              }
+                            }}
+                          />
+                        </Box>
+                      </Td>
                       <Td px={1} py={2}>
                         <Box display="flex" justifyContent="center">
                           {item.Items_images && item.Items_images[0] ? (
